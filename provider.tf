2a8cc56f502b0fb4ae2a5601d1ee9b48042611e0
yeswanthmurugan <yeswanthmurugan3115@gmail.com>
2025-06-14 14:04:40 +0530

    new change

--- provider.tf
+++ provider.tf
@@ -1,24 +1,32 @@
 provider "aws" {
     alias = "logarchive"
   region = "us-east-1"
-
+  assume_role {
+    role_arn = "arn:aws:iam::060236280379:role/teraforrm-role"
+  }
 }
  
 provider "aws" {
   alias = "us-west-1"
   region = "us-west-1"
-
+  assume_role {
+    role_arn = "arn:aws:iam::060236280379:role/teraforrm-role"
+  }
 }
 
 provider "aws" {
   alias = "us-east-2"
   region = "us-east-2"
-
+  assume_role {
+    role_arn = "arn:aws:iam::060236280379:role/teraforrm-role"
+  }
 } 
 
 provider "aws" {
   alias = "us-west-2"
   region = "us-west-2"
-
+  assume_role {
+    role_arn = "arn:aws:iam::060236280379:role/teraforrm-role"
+  }
 }
 
