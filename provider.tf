1b247621dfbe02baea16bc24a8798c0cc12319cb
yeswanthmurugan <yeswanthmurugan3115@gmail.com>
2025-06-15 12:02:38 +0530

    role commit

--- provider.tf
+++ provider.tf
@@ -2,7 +2,8 @@ provider "aws" {
     alias = "logarchive"
   region = "us-east-1"
   assume_role {
-    role_arn = "arn:aws:iam::060236280379:role/teraforrm-role"
+    role_arn          = "arn:aws:iam::060236280379:role/terraform-apply"
+   
   }
 }
  
@@ -10,7 +11,8 @@ provider "aws" {
   alias = "us-west-1"
   region = "us-west-1"
   assume_role {
-    role_arn = "arn:aws:iam::060236280379:role/teraforrm-role"
+    role_arn          = "arn:aws:iam::060236280379:role/terraform-apply"
+   
   }
 }
 
@@ -18,7 +20,8 @@ provider "aws" {
   alias = "us-east-2"
   region = "us-east-2"
   assume_role {
-    role_arn = "arn:aws:iam::060236280379:role/teraforrm-role"
+    role_arn          = "arn:aws:iam::060236280379:role/terraform-apply"
+   
   }
 } 
 
@@ -26,7 +29,8 @@ provider "aws" {
   alias = "us-west-2"
   region = "us-west-2"
   assume_role {
-    role_arn = "arn:aws:iam::060236280379:role/teraforrm-role"
+    role_arn          = "arn:aws:iam::060236280379:role/terraform-apply"
+   
   }
 }
 
